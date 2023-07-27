d4e5f979a3eec3912a776e45176a09e17e0b6c8f
bilal-comsian <bilal.comsian09@gmail.com>
2023-07-27 17:19:20 +0500

    Inital commit

---
+++ IncludeFileInDll/EmbeddedFileClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncludeFileInDll
+{
+    public class EmbeddedFileClass
+    {
+        public static string GetEmbeddedTxtFileContent()
+        {
+            // Replace "YourNamespace" with the namespace where the .txt file is located.
+            string resourceName = "IncludeFileInDll.TestFile.txt";
+
+            // Get the current assembly.
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            // Read the embedded resource and return its content.
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream != null)
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+
+            // If the resource is not found, return null or an empty string, depending on your use case.
+            return null;
+        }        
+    }
+}
---
+++ IncludeFileInDll/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IncludeFileInDll;
+
+Console.WriteLine("Hello, World!");
+
+string data = EmbeddedFileClass.GetEmbeddedTxtFileContent();
+Console.WriteLine(data);
+Console.ReadLine();
